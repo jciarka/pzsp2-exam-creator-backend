7989ad4835e9e15872e43945ae684b5ca3be3784
Jakub Ciarka <01104656@pw.edu.pl>
2022-04-27 16:43:37 +0200

    Deployment files ready

--- src/main/java/com/PZSP2/PFIMJ/controllers/SubjectController.java
+++ src/main/java/com/PZSP2/PFIMJ/controllers/SubjectController.java
@@ -4,18 +4,13 @@ package com.PZSP2.PFIMJ.controllers;
 import com.PZSP2.PFIMJ.projections.SubjectProjection;
 import com.PZSP2.PFIMJ.services.SubjectsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path="api/subjects")
 public class SubjectController {
-
-
     private final SubjectsService subjectsService;
 
     @Autowired
@@ -27,5 +22,4 @@ public class SubjectController {
     public List<SubjectProjection> getUserSubjects(@PathVariable("userId") long userId){
         return this.subjectsService.getUserSubjects(userId);
     }
-
 }
